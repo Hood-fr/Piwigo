405e46b7aa80c8dea038e1e8172ad62ca2f473d0
Robin MORET <robin.moret@supaero.org>
2020-11-23 23:46:28 +0100

    Update languages 2.10

--- language/hu_HU/common.lang.php
+++ language/hu_HU/common.lang.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 /*
 Language Name: Magyar [HU]
-Version: auto
+Version: 2.10.2
 Language URI: http://piwigo.org/ext/extension_view.php?eid=423
 Author: Piwigo team
 Author URI: http://piwigo.org
