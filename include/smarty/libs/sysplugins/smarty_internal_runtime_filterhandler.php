8d05bbb35e30f15fcdc55f3d52ba9fb8f8274b81
Robin MORET <robin.moret@supaero.org>
2022-08-21 12:58:41 +0200

    Correctif bug smarty suite passage v12

--- include/smarty/libs/sysplugins/smarty_internal_runtime_filterhandler.php
+++ include/smarty/libs/sysplugins/smarty_internal_runtime_filterhandler.php
@@ -58,9 +58,14 @@ class Smarty_Internal_Runtime_FilterHandler
             }
         }
         // loop over registered filters of specified type
-        if (!empty($template->smarty->registered_filters[ $type ])) {
-            foreach ($template->smarty->registered_filters[ $type ] as $key => $name) {
-                $content = call_user_func($template->smarty->registered_filters[ $type ][ $key ], $content, $template);
+        if (!empty($template->smarty->registered_filters[$type])) {
+            foreach ($template->smarty->registered_filters[$type] as $key => $name) {
+                if (is_array($template->smarty->registered_filters[$type][$key])) {
+                    $content = call_user_func($template->smarty->registered_filters[$type][$key], $content, $template);
+                } else {
+                    $content = $template->smarty->registered_filters[$type][$key]($content, $template);
+                }
+
             }
         }
         // return filtered output
