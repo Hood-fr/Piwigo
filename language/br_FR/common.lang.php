ae995aa8251b96498f3ab49d032d5ee76d3f917a
Robin MORET <robin.moret@dedaelum.org>
2025-03-08 11:31:47 +0100

    [language] Upgrade 15.4.0

--- language/br_FR/common.lang.php
+++ language/br_FR/common.lang.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 /*
 Language Name: Brezhoneg [FR]
-Version: 15.3.0
+Version: 15.4.0
 Language URI: http://piwigo.org/ext/extension_view.php?eid=698
 Author: Piwigo team
 Author URI: http://piwigo.org
--- language/zh_HK/common.lang.php
+++ language/zh_HK/common.lang.php
@@ -7,7 +7,7 @@
 // +-----------------------------------------------------------------------+
 /*
 Language Name: 中文 (香港) [HK]
-Version: 15.3.0
+Version: 15.4.0
 Language URI: http://piwigo.org/ext/extension_view.php?eid=614
 Author: Piwigo team
 Author URI: http://piwigo.org
